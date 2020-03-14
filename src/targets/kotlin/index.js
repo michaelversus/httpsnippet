a098770a2711d4b8e1223ce904f84f6e66f41b73
Michalis Karagiorgos <michaliskarag@gmail.com>
2020-03-14 17:29:15 +0200

    add custom_kotlin require

---
+++ src/targets/kotlin/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    info: {
+        key: 'kotlin',
+        title: 'kotlin',
+        extname: '.kt',
+        default: 'custom'
+    },
+
+    custom: require('../../../../../custom/custom_kotlin')
+}
